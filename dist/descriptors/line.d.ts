93ca0bb03c786cbfb026aa6d1b328673d7b01ff6
Charles McNulty <charles.mcnulty@tasconline.com>
2025-12-04 11:56:16 -0600

    Updates

--- dist/descriptors/line.d.ts
+++
@@ -1,6 +0,0 @@
-import { Chart2TextOptions } from '../types';
-/**
- * Generates natural language description for line chart segments
- */
-export declare function describeLineChart(xScale: any[], yScale: any[], options: Chart2TextOptions): string;
-//# sourceMappingURL=line.d.ts.map
