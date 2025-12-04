cc854a264e213b987468056913155cddcfb9fc77
Charles McNulty <charles.mcnulty@tasconline.com>
2025-12-04 12:53:31 -0600

    Updates

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * chart2text - Chart.js plugin for accessible text descriptions
+ *
+ * Automatically generates natural language descriptions of chart data
+ * for screen readers and other assistive technologies.
+ *
+ * @packageDocumentation
+ */
+export { chart2text } from './plugin';
+export { Chart2TextOptions, TemplateSet, TrendTemplates, Chart2TextPlugin } from './types';
+export { describeLineChart } from './descriptors/line';
+export { describeBarChart } from './descriptors/bar';
+export { formatNumber } from './formatters/number';
+export { englishTemplates } from './templates/en';
+export { chart2text as default } from './plugin';
+//# sourceMappingURL=index.d.ts.map
