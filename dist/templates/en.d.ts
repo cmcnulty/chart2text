5f03f1e79d0027f265b234d96529b58c83289d9c
Charles McNulty <charles.mcnulty@tasconline.com>
2025-12-04 19:06:01 -0600

    remove dist from git

--- dist/templates/en.d.ts
+++
@@ -1,6 +0,0 @@
-import { TemplateSet } from '../types';
-/**
- * English language templates for chart descriptions
- */
-export declare const englishTemplates: TemplateSet;
-//# sourceMappingURL=en.d.ts.map
